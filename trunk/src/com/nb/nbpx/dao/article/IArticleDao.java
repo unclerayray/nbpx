ec5d3486902a6334499339292384148e145fa4b7
lichao.lei@gmail.com <lichao.lei@gmail.com@3191a346-10bd-685e-199d-95180e439cfc>
2013-06-02 01:18:05 +0000

    增加article模块

---
+++ trunk/src/com/nb/nbpx/dao/article/IArticleDao.java
@@ -0,0 +1,15 @@
+/**
+ * @author Roger
+ */
+package com.nb.nbpx.dao.article;
+
+import com.nb.nbpx.dao.IBaseDao;
+import com.nb.nbpx.pojo.article.Article;
+
+/**
+ * @author Roger
+ * @date 2013-5-11
+ */
+public interface IArticleDao extends IBaseDao<Article, Integer> {
+
+}
