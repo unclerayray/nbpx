00997e58ffa21fbe187ccedc1cbe00a8ed8be689
lichao.lei@gmail.com <lichao.lei@gmail.com@3191a346-10bd-685e-199d-95180e439cfc>
2013-11-07 16:58:35 +0000

    完成需求模块的后台

--- trunk/src/com/nb/nbpx/service/requirement/IRequirementService.java
+++ trunk/src/com/nb/nbpx/service/requirement/IRequirementService.java
@@ -3,6 +3,10 @@ package com.nb.nbpx.service.requirement;
 import com.nb.nbpx.pojo.requirement.Requirement;
 import com.nb.nbpx.service.IBaseService;
 
-public interface IRequirementService extends IBaseService{
+public interface IRequirementService extends IBaseService {
+	public String queryRequirements(Integer hasReplied, Integer rows,
+			Integer start, String sort, String order);
+
+	public void replyRequirement(Integer requirementId,String reply);
 	public void saveRequirement(Requirement requirement);
 }
--- trunk/src/com/nb/nbpx/service/requirement/impl/RequirementServiceImpl.java
+++ trunk/src/com/nb/nbpx/service/requirement/impl/RequirementServiceImpl.java
@@ -1,19 +1,35 @@
 package com.nb.nbpx.service.requirement.impl;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import com.nb.nbpx.common.ResponseStatus;
 import com.nb.nbpx.dao.requirement.IRequirementDao;
+import com.nb.nbpx.pojo.article.Article;
 import com.nb.nbpx.pojo.requirement.Requirement;
-import com.nb.nbpx.service.impl.*;
+import com.nb.nbpx.service.impl.BaseServiceImpl;
 import com.nb.nbpx.service.requirement.IRequirementService;
+import com.nb.nbpx.utils.JsonUtil;
+import com.nb.nbpx.utils.daotool.Equality;
 
 @Component("RequirementService")
 public class RequirementServiceImpl extends BaseServiceImpl implements IRequirementService{
 	
 	private IRequirementDao requirementDao;
+	private JdbcTemplate jdbcTemplate;
+	@Resource
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
+	public JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
 
 	@Override
 	public void saveRequirement(Requirement requirement){
@@ -31,5 +47,30 @@ public class RequirementServiceImpl extends BaseServiceImpl implements IRequirem
 	@Resource
 	public void setRequirementDao(IRequirementDao requirementDao) {
 		this.requirementDao = requirementDao;
+	}
+
+	@Override
+	public String queryRequirements(Integer hasReplied, Integer rows, Integer start,
+			String sort, String order) {
+		String json = "";
+		Map<String, Object> propsMap = this.createPropMap(new Equality(
+				"hasReplied", hasReplied));
+		List<Requirement> list= requirementDao.queryEntityListByProperties(Requirement.class, rows, start, sort, order, propsMap);
+		if (list.isEmpty()) {
+			json = JsonUtil.formatToJsonWithTimeStamp(0,
+					ResponseStatus.SUCCESS, "", list);
+		} else {
+			int count = requirementDao.queryTotalCount(Requirement.class, propsMap)
+					.intValue();
+			json = JsonUtil.formatToJsonWithTimeStamp(count,
+					ResponseStatus.SUCCESS, "", list);
+		}
+
+		return json;
+	}
+
+	@Override
+	public void replyRequirement(Integer requirementId, String reply) {
+		jdbcTemplate.update("UPDATE requirements SET reply = ? and hasReplied = ? WHERE requirementId = ?", new Object[] {reply, true, requirementId}); 
 	}
 }
