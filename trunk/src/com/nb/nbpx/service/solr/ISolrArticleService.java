069b40b782f6c0644052ffbec0f3b79b29e32a4b
Lichao.Lei@gmail.com <Lichao.Lei@gmail.com@3191a346-10bd-685e-199d-95180e439cfc>
2014-08-30 13:47:15 +0000

    修复首页的一些bug

--- trunk/src/com/nb/nbpx/service/solr/ISolrArticleService.java
+++ trunk/src/com/nb/nbpx/service/solr/ISolrArticleService.java
@@ -12,4 +12,6 @@ public interface ISolrArticleService extends IBaseSolrService{
 	public String queryRelatedArticles(String q, Integer start, Integer rows) throws SolrServerException, IOException, NbpxException;
 	public void updateArticleInfo2Solr(Integer articleId) throws IOException, SolrServerException ;
 	public void removeArticleFromSolr(Integer articleId) throws SolrServerException, IOException;
+	public String queryRelatedArticlesForIndex(String q, Integer start, Integer rows)
+			throws SolrServerException, IOException, NbpxException;
 }
